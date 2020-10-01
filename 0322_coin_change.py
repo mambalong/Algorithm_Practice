23e6e8077dd546f22d3b462862db595f57653570
Steve Ren <steveren@Steve-MacBook-Pro.local>
2020-10-01 16:29:25 +0800

    fixed

--- 0322_coin_change.py
+++ 0322_coin_change.py
@@ -69,13 +69,13 @@ def coinChange(coins, amount):
 
 def coinChange(coins, amount):
     dp = [float('inf')] * (amount+1)
-            dp[0] = 0
-            for i in range(0, amount+1):
-                for coin in coins:
-                    if i - coin < 0:
-                        continue
-                    dp[i] = min(dp[i], 1+dp[i-coin])
-            return dp[amount] if dp[amount] != float('inf') else -1
+    dp[0] = 0
+    for i in range(0, amount+1):
+        for coin in coins:
+            if i - coin < 0:
+                continue
+            dp[i] = min(dp[i], 1+dp[i-coin])
+    return dp[amount] if dp[amount] != float('inf') else -1
 
 
 
