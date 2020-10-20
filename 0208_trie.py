2918b6b7f29e354843fe1baac64e0edbcabb7e7a
Steve Ren <steveren@Steve-MacBook-Pro.local>
2020-10-20 18:17:36 +0800

    finish

---
+++ 0208_trie.py
@@ -0,0 +1,68 @@
+'''
+Implement a trie with insert, search, and startsWith methods.
+
+Example:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");   
+trie.search("app");     // returns true
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/implement-trie-prefix-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.trie = {}
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        curr = self.trie
+        for c in word:
+            if c not in curr:
+                curr[c] = {}
+            curr = curr[c]
+        curr['#'] = 1
+            
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        curr = self.trie
+        for c in word:
+            if c not in curr:
+                return False
+            curr = curr[c]
+        return '#' in curr
+
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        curr = self.trie
+        for c in prefix:
+            if c not in curr:
+                return False
+            curr = curr[c]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
